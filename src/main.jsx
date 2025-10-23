b41f7300684f9dbc3b4d1fcda3d237ef05d2093f
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-10-24 04:08:35 +0600

    feat: dynamic slides

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import router from './router/router.jsx'
-import { RouterProvider } from 'react-router'
-import AuthProvider from './contexts/AuthContext/AuthProvider.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./router/router.jsx";
+import { RouterProvider } from "react-router";
+import AuthProvider from "./contexts/AuthContext/AuthProvider.jsx";
+// TanStack Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+// Create QueryClient
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
